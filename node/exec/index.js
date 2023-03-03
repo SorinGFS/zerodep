f8a52c7bad3194a64bb4039f966750d42072542a
SorinGFS <sorin.gfs@protonmail.com>
2023-03-03 15:21:22 +0200

    quote args

--- node/exec/index.js
+++ node/exec/index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 'use strict';
-
+// define vars
 const { exec } = require('child_process');
-
-module.exports = (cmd) => {
-    const child = exec(cmd);
+// executes the command line arguments and returns the exit code
+module.exports = (...args) => {
+    const child = exec(args.reduce((args, arg) => (arg.indexOf(' ') > 0 ? `${args} "${arg}"` : `${args} ${arg}`), ''));
     child.stderr.pipe(process.stderr);
     child.stdout.pipe(process.stdout);
     return new Promise((resolve) => child.on('close', resolve));
