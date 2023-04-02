c625615d4ab9b8c65ebe33a838482ac4f823da37
SorinGFS <sorin.gfs@protonmail.com>
2023-04-02 08:35:00 +0300

    jsonPointer and jsonPointerKeys fns

--- node/fn/index.js
+++ node/fn/index.js
@@ -86,6 +86,17 @@ module.exports = {
         if (!Array.isArray(target)) return {};
         return Promise.resolve(target.reduce((acc, value) => this.mergeDeep(acc, parse(value)), {}));
     },
+    // converts object keys to jsonPointer
+    jsonPointer: (...keys) => keys.map((key) => key.replace(/\\(.)/g, '$1').replaceAll('~', '~0').replaceAll('/', '~1')).join('/'),
+    // converts jsonPointer to object keys
+    jsonPointerKeys: (jsonPointer) => {
+        return jsonPointer.split('/').map((key) =>
+            key
+                .replace(/[\u0022\u005C\u0000-\u001F]/g, '\\$&')
+                .replaceAll('~1', '/')
+                .replaceAll('~0', '~')
+        );
+    },
     // https://gist.github.com/jeneg/9767afdcca45601ea44930ea03e0febf
     // split the object reference by corresponding delimiter and pass the keys array using spread operator
     get: (object, ...keys) => {
